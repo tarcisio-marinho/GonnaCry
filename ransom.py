7f87dbb51048b9ce786b5b365c220685fbe313d7
marinho <tarcisio_marinho09@hotmail.com>
2017-05-12 21:06:06 -0300

    update

---
+++ ransom.py
@@ -0,0 +1,8 @@
+#!/bin/bash/env python
+# coding=UTF-8
+# by Tarcisio marinho
+# github.com/tarcisio-marinho
+
+diretorio= '/'
+
+os.walk(diretorio)
---
+++ user/ransom.py
@@ -0,0 +1,4 @@
+#!/bin/bash/env python
+# coding=UTF-8
+# by Tarcisio marinho
+# github.com/tarcisio-marinho
